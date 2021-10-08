a3c800654769d072f4ca8db7f704e8a0e1cf1512
Khaleel Jageer <jskcse4@gmail.com>
2021-10-08 21:17:05 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'main_page.dart';
+
+void main() {
+  runApp(const KidsStories());
+}
+
+class KidsStories extends StatelessWidget {
+  const KidsStories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MainPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+    );
+  }
+}
