dd5e7cb36fe1a855392d7ff978db58e9d56b779e
kanimozhi <kanimozhi102001@gmail.com>
2021-10-10 12:17:28 +0530

    Home Page Modified

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,8 +10,14 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Main Page"),
+    return Container(
+      child: Center(
+        child: Column(
+          children: [
+            Text("Main Page"),
+          ],
+        ),
+      ),
     );
   }
 }
